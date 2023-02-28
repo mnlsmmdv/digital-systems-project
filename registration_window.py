93440e6679439ba3be73b0a884d35d51b2282d83
mnlsmmdv <mnlsmmdv13@gmail.com>
2023-02-28 16:34:17 +0500

    Function declared with placeholder

--- registration_window.py
+++ registration_window.py
@@ -66,4 +66,9 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE."""
 
+# This function checks all fields have been filled.
+def validate_user_registration():
+    # Placeholder.
+    pass
+
 # PROGRAM END.
