1f3e0bdb01cf199443e7a6b23712057fe539b91a
mnlsmmdv <mnlsmmdv13@gmail.com>
2023-02-28 16:22:55 +0500

    New file created

---
+++ overview_window.py
@@ -0,0 +1,18 @@
+"""
+Name: Ahmed Affaan
+Title: overview_window.py
+Folder: -
+Date: -
+Country: Republic of Maldives
+Code version: -
+Description: Login window for P2P payment application.
+Credits to:
+Credits to link: 
+Note: Uncomment codes to execute and comment them when not in use.
+"""
+
+# PROGRAM START.
+
+
+
+# PROGRAM END.
