1fa6abdc878b5043ca2fcc8f5c9973857dd97aff
mnlsmmdv <mnlsmmdv13@gmail.com>
2023-03-01 23:23:17 +0500

    Centers window on screen center on initial run

--- login_window.py
+++ login_window.py
@@ -34,6 +34,16 @@ login_window.title("Login") # Window title.
 login_window.geometry("800x608") # Window dimensions.
 #login_window.resizable(False, False) # Keeping constant window dimension size.
 
+# Configurations to center login window window on initial run.
+login_window.update() # Refreshes the window.
+login_window_width = login_window.winfo_width() # Retrieves the window width.
+login_window_height = login_window.winfo_height() # Retrieves the window height.
+screen_width = login_window.winfo_screenwidth() # Retrieves the screen width.
+screen_height = login_window.winfo_screenheight() # Retrieves the screen height.
+x_axis = int((screen_width / 2) - (login_window_width / 2)) # Calculates x-axis.
+y_axis = int((screen_height / 2) - (login_window_height / 2)) # Calculates y-axis.
+login_window.geometry(f"{login_window_width}x{login_window_height}+{x_axis}+{y_axis}") # Sets the window size to any screen center.
+
 # Running the main window event loop.
 login_window.mainloop()
 
