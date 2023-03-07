075b62ff27336218bf123709f9483230af03dca7
mnlsmmdv <mnlsmmdv13@gmail.com>
2023-03-08 00:42:23 +0500

    Centers window size to any screen center

--- overview_window.py
+++ overview_window.py
@@ -35,6 +35,16 @@ overview_window.title("Overview") # Window title.
 overview_window.geometry("800x608") # Window dimensions.
 #overview_window.resizable(False, False) # Keeping constant window dimension size.
 
+# Configurations to center overview_window on initial run.
+overview_window.update() # Refreshes the window.
+overview_window_width = overview_window.winfo_width() # Retrieves the window width.
+overview_window_height = overview_window.winfo_height() # Retrieves the window height.
+screen_width = overview_window.winfo_screenwidth() # Retrieves the screen width.
+screen_height = overview_window.winfo_screenheight() # Retrieves the screen height.
+x_axis = int((screen_width / 2) - (overview_window_width / 2)) # Calculates x-axis.
+y_axis = int((screen_height / 2) - (overview_window_height / 2)) # Calculates y-axis.
+overview_window.geometry(f"{overview_window_width}x{overview_window_height}+{x_axis}+{y_axis}") # Sets the window size to any screen center.
+
 # Running the main window event loop.
 overview_window.mainloop()
 
