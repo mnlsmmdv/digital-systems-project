aa3ffb760ce189429189c204984bbed7be0e75a2
mnlsmmdv <mnlsmmdv13@gmail.com>
2023-03-07 23:22:59 +0500

    Centers window size to any screen center

--- request_window.py
+++ request_window.py
@@ -35,6 +35,16 @@ request_window.title("Request") # Window title.
 request_window.geometry("800x608") # Window dimensions.
 #request_window.resizable(False, False) # Keeping constant window dimension size.
 
+# Configurations to center request_window on initial run.
+request_window.update() # Refreshes the window.
+request_window_width = request_window.winfo_width() # Retrieves the window width.
+request_window_height = request_window.winfo_height() # Retrieves the window height.
+screen_width = request_window.winfo_screenwidth() # Retrieves the screen width.
+screen_height = request_window.winfo_screenheight() # Retrieves the screen height.
+x_axis = int((screen_width / 2) - (request_window_width / 2)) # Calculates x-axis.
+y_axis = int((screen_height / 2) - (request_window_height / 2)) # Calculates y-axis.
+request_window.geometry(f"{request_window_width}x{request_window_height}+{x_axis}+{y_axis}") # Sets the window size to any screen center.
+
 # Running the main window event loop.
 request_window.mainloop()
 
