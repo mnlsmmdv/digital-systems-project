7a9f05a7db697bd53fc397115879e0c50231ea67
mnlsmmdv <mnlsmmdv13@gmail.com>
2023-03-31 14:44:11 +0500

    Redundant fields removed

--- request_window.py
+++ request_window.py
@@ -6,8 +6,6 @@ Date: -
 Country: Republic of Maldives
 Code version: -
 Description: Login window for P2P payment application.
-Credits to:
-Credits to link: 
 Note: Uncomment codes to execute and comment them when not in use.
 """
 
